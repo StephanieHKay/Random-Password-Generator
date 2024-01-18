cab63e32898202d035e842964efd282f078b655d
Stephanie Kayode <stephanie.kayode@gmail.com>
2024-01-18 19:05:57 +0000

    improve output spacing

--- random_gen.py
+++ random_gen.py
@@ -7,8 +7,8 @@ import string
 def generate_random_password():
     while True:
         #ask user for lenght and number of passwords and  which characters can make up the password  
-        length = int(input("Enter a required password length: \n"))
-        no_of_passwords = int (input("How many passwords do you want to generate: \n"))
+        length = int(input("Enter a required password length: "))
+        no_of_passwords = int (input("How many passwords do you want to generate: "))
         while True:
             LPD = input("Include letters, numbers and punctuation? yes/no ")
             if LPD[0].lower() == "y":
@@ -29,7 +29,7 @@ def generate_random_password():
             password = "".join(secrets.choice(charac) for n in range(length))
             print(f"Generated Password No {n+1}: \n{password}")
         
-        regenerate = input("Generate new password/s? yes/no ")
+        regenerate = input("\nGenerate new password/s? yes/no ")
         if regenerate[0].lower() == "n":
             print("Generator stopped")
             break
